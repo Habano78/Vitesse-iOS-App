621b6d00717a46994a72c472456413f6d3356f52
William-Gabriel <willx_gabriel@yahoo.com>
2025-06-30 11:06:34 +0200

    Initial Commit

---
+++ Vitesse/VitesseApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitesseApp.swift
+//  Vitesse
+//
+//  Created by Perez William on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitesseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
